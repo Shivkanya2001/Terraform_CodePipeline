e7ce1b20a9b68efdded55b1c3230460862af0758
Shivkanya2001 <shivani.doiphode2001@gmail.com>
2025-09-28 19:20:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_s3_bucket" "devops_on_aws_bucket" {
-    bucket_prefix = "codepipeline-bucket"
+    bucket_prefix = "codepipeline-bucket123456780"
 }
